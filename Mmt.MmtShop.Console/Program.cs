8325dec4b0df9d0ea495fad1a1a3e61c3e557383
Mark <karmacarrot@hotmail.com>
2021-09-04 09:34:55 +0100

    initial check in, project initialisation

---
+++ Mmt.MmtShop.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mmt.MmtShop.ApiConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
